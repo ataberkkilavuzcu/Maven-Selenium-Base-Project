afb3b338e275cd6f68ab5c528358f65a19aef952
Ataberk Kılavuzcu <109624077+ataberkkilavuzcu@users.noreply.github.com>
2023-08-22 21:49:49 +0300

    Update BasePage.java

--- src/main/java/Pages/BasePage.java
+++ src/main/java/Pages/BasePage.java
@@ -4,6 +4,8 @@ import java.time.Duration;
 import java.util.ArrayList;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,7 +14,10 @@ import com.ataberkkilavuzcu.Driver.DriverChrome;
 
 public class BasePage {
     
-    ChromeDriver driver = DriverChrome.getDriver(); 
+    ChromeDriver driver = DriverChrome.getDriver();
+    JavascriptExecutor executor = (JavascriptExecutor) driver;
+    //waiting maximum of 105 seconds for loading page and getting elements from page.
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(105)); 
 
     public BasePage(){
         
@@ -22,9 +27,12 @@ public class BasePage {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.get(url);
     }
-
-    public void click(By by){   
-        driver.findElement(by).click();
+    public void waitUntilClickableAndClick(By by){
+        wait.until(ExpectedConditions.elementToBeClickable(by)).click();
+    }
+    public void click(WebElement element){   
+        scrollIntoView(element);
+        element.click();
     }
     
     public void sendKeys(By by, String text){
@@ -40,6 +48,10 @@ public class BasePage {
         wait.until(ExpectedConditions.visibilityOfElementLocated(by));        
     }
 
+    public void scrollIntoView(WebElement element){
+        executor.executeScript("arguments[0].scrollIntoView();", element);
+    }
+    
     public void switchToTab(int tabIndex){
         ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
         driver.switchTo().window(tabs.get(tabIndex));
